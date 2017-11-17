bb9a2b5ec6659fabe610f2e2075a2edc314b02fe
kebus426 <kemu423@gmail.com>
2017-11-17 17:02:03 +0900

    永続セッションの追加 (9章終了)

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171116140321) do
+ActiveRecord::Schema.define(version: 20171117071341) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
     t.index ["name"], name: "index_users_on_name", unique: true
   end
 
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
@@ -9,6 +10,19 @@ class ActiveSupport::TestCase
   def is_logged_in?
     !session[:user_id].nil?
   end
-  
+
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
   # Add more helper methods to be used by all tests here...
+end
+
+class ActionDispatch::IntegrationTest
+
+  # テストユーザーとしてログインする
+  def log_in_as(user, password: 'password', remember_me: '1')
+    post login_path, params: { session: { name: user.name,
+                                          password: password,
+                                          remember_me: remember_me } }
+  end
 end
